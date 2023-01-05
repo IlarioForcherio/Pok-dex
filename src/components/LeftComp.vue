856a98129b073f9ed7efc9cc7c7e600d3f0dd9d3
ilarioforcherio <ilarioforcherio@gmail.com>
2023-01-05 09:43:03 +0100

    import axios - bootstrap

---
+++ src/components/LeftComp.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="text-danger" >
+PORCODIO
+    </div>
+</template>
+
+<script>
+    export default {
+      name:'LeftComp',
+      components:{
+
+      },
+      props:{
+
+      },
+      data(){
+        return{
+
+        }
+      },
+      mounted(){
+
+      },
+      methods(){
+
+      },
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
