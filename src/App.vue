d63db984e4229f947d170c06b47d7c19aa504b44
ilarioforcherio <ilarioforcherio@gmail.com>
2023-01-17 21:33:47 +0100

    corrections

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,10 @@
     <div id="app">
     
         <div class="d-flex justify-content-center">
-            <LeftComp @emitInputTextLeftComp="searchPokemon" :singlePokemon="singlePokemon" />
-            <RightComp :singlePokemon="singlePokemon" />
+            <LeftComp @emitInputTextLeftComp="searchPokemon" :singlePokemon="singlePokemon" :descriptionArrayAppvue='descriptionArray' />
+            <RightComp :singlePokemon="singlePokemon"  />
+              <div>{{description}}</div>
         </div>
-    
-    
     </div>
 </template>
 
@@ -23,39 +22,74 @@ export default {
     },
     data() {
         return {
+            // input che riceve informazioni da leftComp
             inputTextAppue: '',
+            // oggetto pokemon derivante da chiamata API
             singlePokemon: {},
+            //array generale di tutte le descizioni
+            descriptionArray: [],
+            // stringa descrizione desiderata
+            description:'',
+            //stringa con url immagine
+            pokeimg:'',
         }
     },
     mounted() {
         this.searchPokemon()
       
+
     },
     methods: {
 
 
 
         searchPokemon(inputTextLeftComp) {
-           
-           this.inputTextAppue = inputTextLeftComp
 
-            if (this.inputTextAppue == " ") {
-                axios.get( " https://pokeapi.co/api/v2/pokemon/bulbasaur/?limit=151" )
-                .then((resp) => {
-                    this.singlePokemon = resp.data
-                })
+            this.inputTextAppue = inputTextLeftComp
+
+            if (this.inputTextAppue == undefined) {
+                
+                // axios.get(" https://pokeapi.co/api/v2/pokemon/bulbasaur/")
+                //     .then((resp) => {
+                //         this.singlePokemon = resp.data
+                //         //console.log(this.singlePokemon)
+                //     })
+
+                return
             } else {
 
-                axios.get("https://pokeapi.co/api/v2/pokemon/" + this.inputTextAppue + "/?limit=151")
+                axios.get("https://pokeapi.co/api/v2/pokemon/" + this.inputTextAppue)
                     .then((response) => {
-
+                       
+                       //oggetto -> singolo pokemon in base all'input utente
                         this.singlePokemon = response.data
-                        //console.log(this.singlePokemon)
+                       
+                       //variabile pokeimg contiene url dell'immagine
+                       this.pokeimg =  this.singlePokemon.sprites.front_shiny
+                       
                     })
             }
 
+              this.getDescription(inputTextLeftComp)
+
 
         },
+
+
+        getDescription(inputTextLeftComp) {
+
+           
+                    
+                     axios.get("https://pokeapi.co/api/v2/pokemon-species/" + inputTextLeftComp + "/")
+                    .then((response) => {
+                        //array dei testi
+                        this.descriptionArray = response.data.flavor_text_entries
+                       ////stringa attribuita a variabile description
+                        this.description = this.descriptionArray[1].flavor_text
+                        console.log(this.description);
+                    })
+
+        }
 
     },
 
